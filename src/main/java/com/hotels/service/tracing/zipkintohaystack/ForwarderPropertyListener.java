bd5b6d3d4f08627fd1ac0d41f150574b8bedffd3
andystanton <andystanton@gmail.com>
2018-11-25 20:18:39 +0000

    Fail fast when no forwarder is configured

--- src/main/java/com/hotels/service/tracing/zipkintohaystack/ForwarderPropertyListener.java
+++ src/main/java/com/hotels/service/tracing/zipkintohaystack/ForwarderPropertyListener.java
@@ -2,6 +2,7 @@ package com.hotels.service.tracing.zipkintohaystack;
 
 import org.springframework.boot.context.event.ApplicationEnvironmentPreparedEvent;
 import org.springframework.context.ApplicationListener;
+import org.springframework.lang.NonNull;
 
 import java.util.Map;
 
@@ -11,7 +12,7 @@ public class ForwarderPropertyListener implements ApplicationListener<Applicatio
     private static final String FORWARDER_ENABLED_PATTERN = "^pitchfork\\.forwarders\\..+\\.enabled$";
 
     @Override
-    public void onApplicationEvent(ApplicationEnvironmentPreparedEvent event) {
+    public void onApplicationEvent(@NonNull ApplicationEnvironmentPreparedEvent event) {
         Map<String, Object> systemProperties = event.getEnvironment().getSystemProperties();
         if (systemProperties.entrySet().stream()
                 .noneMatch(property -> property.getKey().matches(FORWARDER_ENABLED_PATTERN) && TRUE.toString().equals(property.getValue()))) {
