fa80d89a79d6e08d8846be2843d09ca0bd8ccad6
nsahai8 <nihgupta@expedia.com>
2019-08-04 11:17:19 +0530

    disabling the auto configure

--- src/main/java/com/hotels/service/tracing/zipkintohaystack/metrics/JmxReporterService.java
+++ src/main/java/com/hotels/service/tracing/zipkintohaystack/metrics/JmxReporterService.java
@@ -1,22 +1,30 @@
 package com.hotels.service.tracing.zipkintohaystack.metrics;
 
-import com.codahale.metrics.JmxReporter;
+
+import com.codahale.metrics.jmx.JmxReporter;
+
+
 import io.micrometer.core.instrument.Clock;
 import io.micrometer.core.instrument.MeterRegistry;
-import io.micrometer.core.instrument.util.HierarchicalNameMapper;
 import io.micrometer.jmx.JmxConfig;
 import io.micrometer.jmx.JmxMeterRegistry;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
-import javax.inject.Inject;
-
 
 @Component
 public class JmxReporterService {
 
-    @Inject
-    public void JmxReporterService( JmxMeterRegistry jmxMeterRegistry ) {
+
+    private  JmxMeterRegistry jmxMeterRegistry;
+
+
+    @Bean
+    public MeterRegistry JmxReporterService( Clock clock) {
+        this.jmxMeterRegistry =  new JmxMeterRegistry(JmxConfig.DEFAULT, clock);
+        System.out.println("JmxReporterService: "+jmxMeterRegistry);
         JmxReporter.forRegistry(jmxMeterRegistry.getDropwizardRegistry()).build().start();
+        return this.jmxMeterRegistry;
     }
+
 }
