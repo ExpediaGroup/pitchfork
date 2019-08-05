84a8467d1dd96b2ed8101413a9404229883f6254
nsahai8 <nihgupta@expedia.com>
2019-08-05 13:22:33 +0530

    adding docker files

--- build/docker/start-app.sh
+++ build/docker/start-app.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 
 [ -z "$JAVA_XMS" ] && JAVA_XMS=1024m
@@ -7,16 +6,24 @@
 
 set -e
 JAVA_OPTS="${JAVA_OPTS} \
--javaagent:${APP_HOME}/${JMXTRANS_AGENT}.jar=${APP_HOME}/jmxtrans-agent.xml \
+-javaagent:/pitchfork/${JMXTRANS_AGENT}.jar=/pitchfork/jmxtrans-agent.xml \
 ${JAVA_GC_OPTS} \
 -Xmx${JAVA_XMX} \
 -Xms${JAVA_XMS} \
 -XX:+ExitOnOutOfMemoryError \
--Dapplication.name=${APP_NAME} \
--Dapplication.home=${APP_HOME}"
+-Dapplication.name=pitchfork \
+-Dapplication.home=/pitchfork"
 
 if [[ -n "$SERVICE_DEBUG_ON" ]] && [[ "$SERVICE_DEBUG_ON" == true ]]; then
    JAVA_OPTS="$JAVA_OPTS -Xdebug -Xrunjdwp:transport=dt_socket,address=5005,server=y"
 fi
 
+exec /opt/jdk-mini/bin/java ${JAVA_OPTS} -jar pitchfork.jar
 
+#-javaagent:/pitchfork/jmxtrans-agent-1.2.6.jar=/pitchfork/jmxtrans-agent.xml -XX:+UseG1GC -Xmx1024m -Xms1024m -XX:+ExitOnOutOfMemoryError -Dapplication.name=pitchfork -Dapplication.home=/pitchfork
+
+#CMD export TZ=$(date +%Z) &&\
+#    exec $JAVA_HOME/bin/java \
+#         ${JAVA_OPTS} \
+#         -jar \
+#         pitchfork.jar
